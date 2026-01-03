95dcc767ad7a90f3e685b45331ecaf001804102f
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-04 04:09:26 +0600

    update

--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -8,7 +8,7 @@ const HeroComp = () => {
         <main>
             <div id="hero" className='min-h-screen bg-linear-to-br from-purple-50 via-orange-50 to-transparent'>
                 {/* from-blue-400 to-purple-600 */}
-                <div id="hero-container" className='max-w-4xl mx-auto px-6 pb-32 flex flex-col'>
+                <div id="hero-container" className='max-w-4xl mx-auto px-6 pt-6 pb-32 flex flex-col'>
                     {/* VERSION BOX */}
                     <div id="version-text" className='my-6 flex gap-2 items-center border border-yellow-300 bg-yellow-50 rounded-lg px-3 py-1 w-fit shadow-md hover:shadow-lg hover:-translate-y-1 transition group'>
                         <div className='w-2 h-2 bg-yellow-400 rounded-full border border-yellow-600'></div>
@@ -16,7 +16,12 @@ const HeroComp = () => {
                         <FontAwesomeIcon icon={faArrowRight} className='text-yellow-600 group-hover:translate-x-1 transition duration-300' />
                     </div>
 
-                    
+                    <h1 className='text-4xl font-semibold leading-snug mt-4'>Web app to desktop app in minutes</h1>
+                    <p className='text-xl mt-4'>Take your web app codebase and transform it into a cross platform desktop app with native functionality.</p>
+                    <div id="buttons-container" className='mt-12 flex flex-col sm:flex-row gap-4'>
+                        <button className='bg-primary text-white font-semibold px-8 py-3 rounded-lg hover:opacity-90 '>Downlaod Now</button>
+                        <button className='bg-white font-semibold px-8 py-3 rounded-lg border border-gray-300'>Read Docs</button>
+                    </div>
                 </div>
 
             </div>
