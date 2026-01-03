4873f0f0a4b2e5f6e20b382f4813c5d29359f05f
Rajkumar Sarker <sardonic100@gmail.com>
2026-01-04 01:43:43 +0600

    Update HeroComp.jsx

--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -4,7 +4,7 @@ const HeroComp = () => {
   return (
     <div>
         <main>
-            <div id="hero" className='min-h-screen bg-linear-to-br from-blue-400 to-purple-600'>
+            <div id="hero" className='min-h-screen bg-linear-to-br from-purple-50 via-orange-50 to-transparent'>
                 {/* from-blue-400 to-purple-600 */}
 
             </div>
